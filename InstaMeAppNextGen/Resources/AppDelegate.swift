59e314eaa6b89342dba3dee91154dd14276174a4
Anton Veldanov <anton.veldanov@gmail.com>
2021-08-23 19:22:20 -0700

    iOS

--- InstaMeAppNextGen/Resources/AppDelegate.swift
+++ InstaMeAppNextGen/Resources/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Anton Veldanov on 8/22/21.
 //
 
+import Firebase
 import UIKit
 
 @main
@@ -13,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
