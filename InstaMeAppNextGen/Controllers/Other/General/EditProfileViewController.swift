6bc4646403d5e1c18e31d8df6163e85d1c900ef9
Anton Veldanov <anton.veldanov@gmail.com>
2021-09-05 21:16:06 -0700

    iOS

--- InstaMeAppNextGen/Controllers/Other/General/EditProfileViewController.swift
+++ InstaMeAppNextGen/Controllers/Other/General/EditProfileViewController.swift
@@ -18,7 +18,7 @@ final class EditProfileViewController: UIViewController {
     
     private let tableView: UITableView = {
         let tableView = UITableView()
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.register(FormTableViewCell.self, forCellReuseIdentifier: "cell")
         return tableView
     }()
     
