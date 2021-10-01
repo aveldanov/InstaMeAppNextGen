b0c7363990227e29bd718c7881dc484acfb6f14f
Anton Veldanov <anton.veldanov@gmail.com>
2021-09-30 20:42:50 -0700

    iOS

--- InstaMeAppNextGen/Controllers/Core Tabs/HomeViewController.swift
+++ InstaMeAppNextGen/Controllers/Core Tabs/HomeViewController.swift
@@ -37,11 +37,12 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        createMockModels()
         view.addSubview(tableView)
         tableView.delegate = self
         tableView.dataSource = self
     }
-
+    
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -52,6 +53,48 @@ class HomeViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         handleNotAuthenticated()
+    }
+    
+    
+    private func createMockModels(){
+        let user = User(username: "@anton",
+                        bio: "",
+                        name: (first: "Anton", last: "V"),
+                        profilePhoto: URL(string: "https://www.google.com")!,
+                        birthDate: Date(),
+                        gender: .other,
+                        counts: UserCount(followers: 1, following: 1, posts: 1),
+                        joinDate: Date())
+        let post = UserPost(postID: "",
+                            postType: .photo,
+                            thumbNailImageURL: URL(string: "https://www.google.com")!,
+                            postURL: URL(string: "https://www.google.com")!,
+                            caption: nil,
+                            likeCount: [],
+                            comments: [],
+                            createdDate: Date(),
+                            owner: user,
+                            taggedUsers: [])
+        
+        var comments = [PostComment]()
+        
+        for i in 0..<2{
+            comments.append(PostComment(commentID: "\(i)",
+                                        username: "@jenny",
+                                        text: "Good post",
+                                        createdDate: Date(),
+                                        likes: []))
+        }
+        
+        for i in 0..<5{
+            let viewModel = HomeFeedRenderViewModel(header: PostRenderViewModel(renderType: .header(provider: user)),
+                                                    post: PostRenderViewModel(renderType: .primaryContent(provider: post)),
+                                                    actions: PostRenderViewModel(renderType: .actions(provider: "")),
+                                                    comments: PostRenderViewModel(renderType: .comments(comments: comments)))
+            feedRenderModels.append(viewModel)
+            
+        }
+        
     }
     
     
@@ -76,6 +119,7 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // instead of rows I use subSections
         let sect = section
         let model: HomeFeedRenderViewModel
         if sect == 0{ // as we can't devide by 0
@@ -104,11 +148,16 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
             switch commentModel.renderType{
             case .comments(let comments):
                 return comments.count > 2 ? 2 : comments.count
+                
+                
+            case .header, .primaryContent, .actions: return 0
                 //unknown enforces switch to be exhaustive. Gives in code warning
-            @unknown default:
-                fatalError("Invalid case")
+//            @unknown default:
+//                fatalError("Invalid case")
             }
         }
+        
+        return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -130,32 +179,29 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
         if subSection == 0{
             // header
             let headerModel = model.header
-        
+            
             switch headerModel.renderType{
             case .header(let user):
                 let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostGeneralTableViewCell.identifier, for: indexPath) as! IGFeedPostGeneralTableViewCell
-            @unknown default:
-                fatalError("Invalid case")
+            case .comments, .primaryContent, .actions: return UITableViewCell()
             }
         }else if subSection == 1{
             //post
             let postModel = model.post
             
-            switch .primaryContent.renderType{
+            switch postModel.renderType{
             case .primaryContent(let post):
-                let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostGeneralTableViewCell.identifier, for: indexPath) as! IGFeedPostGeneralTableViewCell
-            @unknown default:
-                fatalError("Invalid case")
+                let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostHeaderTableViewCell.identifier, for: indexPath) as! IGFeedPostHeaderTableViewCell
+            case .header, .comments, .actions: return UITableViewCell()
             }
         }else if subSection == 2{
             //actions
             let actionModel = model.actions
             
-            switch commentModel.renderType{
-            case .comments(let comments):
-                let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostGeneralTableViewCell.identifier, for: indexPath) as! IGFeedPostGeneralTableViewCell
-            @unknown default:
-                fatalError("Invalid case")
+            switch actionModel.renderType{
+            case .actions(let provider):
+                let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostActionsTableViewCell.identifier, for: indexPath) as! IGFeedPostActionsTableViewCell
+            case .header, .comments, .primaryContent: return UITableViewCell()
             }
             
             
@@ -172,26 +218,8 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
             
         }
         
-        
-        
-        
-        
-        
-        switch model.renderType{
-        case .actions(let actions):
-            let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostActionsTableViewCell.identifier, for: indexPath) as! IGFeedPostActionsTableViewCell
-            return cell
-        case .comments(let comments):
-            let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostGeneralTableViewCell.identifier, for: indexPath) as! IGFeedPostGeneralTableViewCell
-            return cell
-        case .primaryContent(let post):
-            let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostTableViewCell.identifier, for: indexPath) as! IGFeedPostTableViewCell
-            return cell
-        case .header(let user):
-            let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostHeaderTableViewCell.identifier, for: indexPath) as! IGFeedPostHeaderTableViewCell
-            return cell
-        }
-        
+        return UITableViewCell()
+
     }
     
     
@@ -201,19 +229,27 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
     
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+                
+        let subSection = indexPath.section%4
         
-        let model = renderModels[indexPath.section]
-        
-        switch model.renderType{
-        case .actions(_):
-            return 60
-        case .comments(_):
-            return 50
-        case .primaryContent(_):
-            return tableView.width
-        case .header(_):
+        if subSection == 0{
+            //header
             return 70
+        }else if subSection == 1{
+            //post
+            return tableView.width
+        }else if subSection == 2{
+            //action
+            return 60
+        }else if subSection == 3{
+            //comment
+            return 50
+        }else{
+            return 0
         }
+        
+        
+
     }
     
 }
