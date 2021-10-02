2e9c3f11239ba0fdd85a6266a5b3bbe413e35ae5
Anton Veldanov <anton.veldanov@gmail.com>
2021-10-01 18:47:51 -0700

    iOS

--- InstaMeAppNextGen/Views/Cells/HomeFeed&Posts/IGFeedPostActionsTableViewCell.swift
+++ InstaMeAppNextGen/Views/Cells/HomeFeed&Posts/IGFeedPostActionsTableViewCell.swift
@@ -13,30 +13,44 @@ class IGFeedPostActionsTableViewCell: UITableViewCell {
     
     private let likeButton: UIButton = {
         let button = UIButton()
-        
-        
+        button.setImage(UIImage(systemName: "heart"), for: .normal)
+        button.tintColor = .label
         return button
     }()
     
+    private let commentButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "message"), for: .normal)
+        button.tintColor = .label
+        return button
+    }()
+    
+    private let sendButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "send"), for: .normal)
+        button.tintColor = .label
+        return button
+    }()
     
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.backgroundColor = .systemGreen
-
+        contentView.addSubview(likeButton)
+        contentView.addSubview(commentButton)
+        contentView.addSubview(sendButton)
+        
+        likeButton.addTarget(self, action: #selector(didTapLikeButton), for: .touchUpInside)
+        commentButton.addTarget(self, action: #selector(didTapCommentButton), for: .touchUpInside)
+        sendButton.addTarget(self, action: #selector(didTapSendButton), for: .touchUpInside)
+        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    public func configureIGFeedCell(){
-        // configure cell
-        
-        
-        
-        
-    }
+
     
     override func layoutSubviews() {
         super.layoutSubviews()
@@ -44,5 +58,32 @@ class IGFeedPostActionsTableViewCell: UITableViewCell {
     
     override func prepareForReuse() {
         super.prepareForReuse()
+    }
+    
+     //MARK: Actions
+    
+    
+    @objc func didTapLikeButton(){
+        
+        
+    }
+    
+    @objc func didTapCommentButton(){
+        
+        
+    }
+    
+    @objc func didTapSendButton(){
+        
+        
+    }
+    
+    
+    
+    
+    public func configureIGFeedCell(){
+        // configure cell
+
+        
     }
 }
